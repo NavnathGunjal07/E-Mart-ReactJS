275dced87b9f835ade6861a3a38638bacf1f1f2b
NavnathGunjal07 <53387548+NavnathGunjal07@users.noreply.github.com>
2022-04-09 02:37:18 +0530

    created e-mar react app

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Products from "./Products";
+
+function Home() {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0">
+        <img src="/assets/bg1.jpg" className="card-img" alt="Baclground" height="550"/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <p className="card-text">
+            CHECK OUT ALL THE TRENDS
+          </p>
+            </div>
+          
+          
+        </div>
+      </div>
+      <Products/>
+    </div>
+  );
+}
+
+export default Home;
